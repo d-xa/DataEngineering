d8ebc1a44d4bb45f66b1f49986ba407c754515d0
anh <anhdo@gmx.net>
2020-08-24 22:40:08 +0200

    Archetype: simple-scala-spark

---
+++ Archetypes/simple-scala-spark/src/main/scala/spark/SparkApp.scala
@@ -0,0 +1,35 @@
+package spark
+
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object SparkApp {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder
+      .appName("Simple Application")
+      .config("spark.master", "local")
+      .getOrCreate()
+
+    spark.sparkContext.setLogLevel("ERROR")
+
+    val sc = spark.sparkContext
+
+    def createRDD(): RDD[(Int,String)] = {
+      val rdd = sc.parallelize(Seq((1, "Spark"), (2, "Scala"), (3, "Big Data")))
+      return rdd
+    }
+
+    val aRDD = createRDD()
+
+    import spark.implicits._
+    val df = aRDD.toDF("Id", "Name")
+
+    df.show()
+
+    spark.stop()
+  }
+}
